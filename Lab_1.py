dda758a490234a18dccd2b1d73e4a5b47b324ee1
thbeca-30 <nmcvetkov@yandex.ru>
2021-02-25 18:22:41 +0300

    work on complex numbers

--- Lab_1.py
+++ Lab_1.py
@@ -1,1 +1,20 @@
-from sympy import diff, symbols
+from sympy import *
+import math
+
+
+def QuadraticSolution(a, b, c):
+ #   a = float(a)
+ #   b = float(b)
+ #   c = float(c)
+    discr = b ** 2 - 4 * a * c
+    print(discr)
+
+
+x, y, y1, y2 = symbols('x y y1 y2')
+y = x**3 + 0.2 * x**2 + 0.5 * x - 1.2 # изначально заданная функция
+y1 = diff(y) # берем производную 1 порядка
+print("y' = ", y1)
+sol = solve(y1,x)
+print(sol)
+#y1 = Poly(diff(y))
+#QuadraticSolution(y1.coeffs()[0], y1.coeffs()[1], y1.coeffs()[2])
